4295270cf97e908e36558de945ced1d230110679
Vincent Sengel <vincent.sengel@gmail.com>
2022-02-18 15:56:19 +0100

    commit de milieu de journée, terminé la méthode delete et ajouté une partie du css

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :movies, only: [ :index ]
-  resources :lists, only: [ :index, :show, :new, :create ] do
-    resources :bookmarks, only: [ :index, :create, :new, :destroy ]
+  root to: 'lists#index'
+  resources :lists, only: [ :show, :new, :create ] do
+    resources :bookmarks, only: [ :create, :destroy ]
   end
 
 end
